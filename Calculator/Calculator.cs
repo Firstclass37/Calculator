c0a0c31fda8a83f7f146bd47a98dbe1edabe15e3
Ivan <firstclass37@mail.ru>
2016-05-05 07:35:14 +0300

    UnknowmChanges

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -8,19 +8,31 @@ namespace Calculator
 {
     public class Calculator
     {
-        public int Sum(int inpu1, int input2)
+        public double AutoCalc(double arg1, double arg2,Operations operation)
+        {
+            switch (operation)
+            {
+                case Operations.Sum: return Sum(arg1, arg2);
+                case Operations.Sub: return Sub(arg1, arg2);
+                case Operations.Dev: return Dev(arg1, arg2);
+                case Operations.Mul: return Mul(arg1, arg2);
+            }
+            return 0;
+
+        }
+        public double Sum(double inpu1, double input2)
         {
             return inpu1 + input2;
         }
-        public int Sub(int inpu1, int input2)
+        public double Sub(double inpu1, double input2)
         {
             return inpu1 - input2;
         }
-        public int Dev(int inpu1, int input2)
+        public double Dev(double inpu1, double input2)
         {
             return inpu1 / input2;
         }
-        public int Mul(int inpu1, int input2)
+        public double Mul(double inpu1, double input2)
         {
             return inpu1 * input2;
         }
