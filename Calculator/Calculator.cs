47fbde136cf7605cba689d63f3172099506f8c2a
Ivan <firstclass37@mail.ru>
2016-05-05 06:38:43 +0300

    FirstVer.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Sum(int inpu1, int input2)
+        {
+            return inpu1 + input2;
+        }
+        public int Sub(int inpu1, int input2)
+        {
+            return inpu1 - input2;
+        }
+        public int Dev(int inpu1, int input2)
+        {
+            return inpu1 / input2;
+        }
+        public int Mul(int inpu1, int input2)
+        {
+            return inpu1 * input2;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,25 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Calculator calculator = new Calculator();
+            string inputString = String.Empty;
+            while ((inputString = Console.ReadLine()) != null)
+            {
+                int result = 0;
+                string[] stringArg = inputString.Split(new Char[] { '-', '+', '/', '*' });
+                int arg1 = 0;
+                int arg2 = 0;
+                if (int.TryParse(stringArg[0], out arg1) && int.TryParse(stringArg[1], out arg2))
+                {
+                    if (inputString.Contains("-")) { Console.WriteLine("Result: " + calculator.Sub(arg1, arg2).ToString()); continue; }
+                    if (inputString.Contains("+")) { Console.WriteLine("Result: " + calculator.Sum(arg1, arg2).ToString()); continue; }
+                    if (inputString.Contains("/")) { Console.WriteLine("Result: " + calculator.Dev(arg1, arg2).ToString()); continue; }
+                    if (inputString.Contains("*")) { Console.WriteLine("Result: " + calculator.Mul(arg1, arg2).ToString()); continue; }                   
+                }
+                else Console.WriteLine("Неккоректный ввод! Попробуйте снова!)");
+
+            }
         }
+
     }
 }
